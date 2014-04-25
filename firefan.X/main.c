4daf06549c7666a23f3c1799fa81a00e8846ef89
Diego <santinelli.diego@gmail.com>
2014-04-25 09:35:51 +0200

    main project update

---
+++ firefan.X/main.c
@@ -0,0 +1,20 @@
+/* 
+ * File:   main.c
+ * Author: diego
+ *
+ * Created on 15 aprile 2014, 19.15
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void main(void) {
+
+    while(1)
+    {
+
+
+    }
+}
+
---
+++ firefan.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=firefan.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/firefan.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=firefan.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/firefan.x.tar
